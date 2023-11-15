db8697c3ec94ef2e5bfd949b1de55aadc2d4553a
Wiam-e <wiam.errabih@gmail.com>
2023-11-15 14:14:36 -0800

    Tester

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on my MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Database used
+USE hbtn_0d_usa;
+-- Create a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
