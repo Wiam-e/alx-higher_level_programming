7413125bd33f9d8cbba96b91f441228a3ef8639a
Wiam-e <wiam.errabih@gmail.com>
2024-01-05 07:37:35 -0800

    Testing 2.0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A script that finds a peak in a list of unsorted integers"""
+"""A script that finds a peak in a list of unsorted integers
+"""
 
 def find_peak(list_of_integers):
 
