be081cb8ab1b4ce549132cc849e1efd60967ca88
Wiam-e <wiam.errabih@gmail.com>
2024-01-05 07:46:32 -0800

    Find a peak problem 1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""A script that finds a peak in a list of unsorted integers
+"""A script that finds a  peak in a list of unsorted integers
 """
 
 def find_peak(list_of_integers):
-
+    """BRUTE force implementation for question
+    """
     max_i = None
-    for ele in list_of_integers:
-        if max_i is None or max_i < ele:
-            max_i = ele
+    for element in list_of_integers:
+        if max_i is None or max_i < element:
+            max_i = element
     return max_i
